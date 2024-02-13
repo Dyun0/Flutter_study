9a1205e22d45ce246b2c61fe781717c5a0ac0177
KDY <yun0368@naver.com>
2024-02-13 22:57:10 +0900

    day4

---
+++ Day04flutterstart/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello world',
+          ),
+        ),
+      ),
+
+    ),
+  );
+}
